4bb5c47a3f84cfa865cc460d161fbc6c3c81c5f7
ducoffeM <melanie.ducoffe@airbus.com>
2025-07-08 16:27:55 +0200

    Apply linters

--- tests/test_cubic.py
+++ tests/test_cubic.py
@@ -1,14 +1,15 @@
-from conftest import test_polynomial_root_calculation_3rd_degree
 import pytest
+from conftest import test_polynomial_root_calculation_3rd_degree
+
 
 @pytest.mark.parametrize(
     "a, b, c, d",
     [
         (1, -6, 11, -6),  # Roots: 1, 2, 3
-        (1, -3, 3, -1),   # Roots: 1, 1, 1
-        (2, -4, 2, -1),   # Roots: 0.5, 1.0, 1.0
-        (1, 0, -4, 4),    # Roots: -2.0, 2.0
-    ]
+        (1, -3, 3, -1),  # Roots: 1, 1, 1
+        (2, -4, 2, -1),  # Roots: 0.5, 1.0, 1.0
+        (1, 0, -4, 4),  # Roots: -2.0, 2.0
+    ],
 )
 def test_cubic(a, b, c, d):
     """
